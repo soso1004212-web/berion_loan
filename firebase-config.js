57fc063ca9d4ead94413c7a1233e058e61a94235
soso1004212-web <soso1004212@gmail.com>
2026-07-14 15:27:33 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCOo2CdsGGi1S3_WEYA5JmxhF-7yYqyhpU",
+  authDomain: "berion-loan.firebaseapp.com",
+  projectId: "berion-loan",
+  storageBucket: "berion-loan.firebasestorage.app",
+  messagingSenderId: "720393778220",
+  appId: "1:720393778220:web:a94712dc57e30eed7f0d89",
+  measurementId: "G-KW9GBDFSMN"
+};
